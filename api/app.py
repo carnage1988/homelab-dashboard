74280d57aea1c494af7bd20fa90534e564c8d1b4
Gavin Gillespie <gavingill1988@gmail.com>
2026-07-08 14:14:03 +0100

    created an index page for front end and added CORS support for dashboard frontend

--- api/app.py
+++ api/app.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 import subprocess
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 def get_health():
     return {"status": "ok"}
